ada48feb7de5f586924eb929155a0c0e76e7198e
stakbucks <dhassidu11@gmail.com>
2023-05-25 13:25:01 +0900

    주석

--- B811216_황태환/과제3/code/LogInControl.cpp
+++
@@ -1,7 +0,0 @@
-#include "LogInControl.h"
-
-bool LogInControl::logIn(string id, string pw, Member* currentUser){
-
-	return currentUser->logIn(id, pw);
-
-}
---
+++ B811216_황태환/과제3/code/LogOutControl.cpp
@@ -0,0 +1,8 @@
+#include "LogOutControl.h"
+
+//로그아웃 
+void LogOutControl::logOut(Member* currentUser){ 
+
+	currentUser->logOut();
+
+}
